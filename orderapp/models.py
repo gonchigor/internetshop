c007def580c61c3f331b5827b256e645f46db671
gonchigor <gonchigor@tut.by>
2019-05-21 12:27:36 +0300

    add help text for order model's fields

--- orderapp/models.py
+++ orderapp/models.py
@@ -13,10 +13,12 @@ class Order(models.Model):
                              related_query_name='order')
     status = models.ForeignKey(OrderStatus, on_delete=models.PROTECT, verbose_name='статус', related_name='order',
                                related_query_name='order')
-    name = models.CharField(verbose_name='имя', max_length=50)
-    phone = models.CharField(max_length=15, verbose_name='телефон')
-    email = models.EmailField(verbose_name='email', blank=True, null=True)
-    delivery_adress = models.TextField(verbose_name='Адрес доставки')
+    name = models.CharField(verbose_name='имя', max_length=50, help_text='Как к вам обращаться?')
+    phone = models.CharField(max_length=15, verbose_name='телефон', help_text='Телефон для связи: +375(29)000-00-00')
+    email = models.EmailField(verbose_name='email', blank=True, null=True,
+                              help_text='Электронная почта: example@domain.com')
+    delivery_adress = models.TextField(verbose_name='Адрес доставки',
+                                       help_text='Пример: г. Минск, ул. Звездная, д. 1, кв. 7')
     comments = models.TextField(verbose_name='комментарий', blank=True, null=True)
     date_create = models.DateTimeField("Дата создания", auto_now_add=True, )
     date_update = models.DateTimeField("Дата последнего изменения", auto_now=True)
